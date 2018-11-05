269aadd3c010421c9b225b44a40a40d25f6f477d
zentus <zentus@cimantra.com>
2018-11-05 02:19:12 +0100

    Add type checking

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const doTimes = (times, callback, constructor = false) => {
 
+	if (typeof times !== 'number') {
+		throw 'do-times expected the first argument to be a number'
+	}
+
+	if (typeof callback !== 'function') {
+		throw 'do-times expected the second argument to be a function'
+	}
+
+	if (!!constructor && typeof constructor !== 'boolean') {
+		throw 'do-times expected the third argument to be a boolean'
+	}
+
 	const doThis = (t, c, ...args) => {
 		for (let i = 0; i < t; i++) {
 			c(i+1, i, ...args);

39f2bb66af445d1c0c9368b29b30c7269ae79356
zentus <zentus@cimantra.com>
2019-04-27 04:42:08 +0200

    Simplify API
    Breaking change:
    * Remove additional arguments.
    * Reverse result of third argument (from constructor to immediatelyInvoked). Default is still to invoke the function immediately.
    * Code style.

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-const loop = (times, callback, ...args) => {
-	for (let index = 0; index < times; index++) {
-		callback(index + 1, index, ...args);
-	}
+const loop = (times, callback) => {
+  for (let time = 1; time <= times; time++) {
+    callback(time, time - 1)
+  }
 }
 
-const doTimes = (times, callback, constructor = false) => {
+const doTimes = (times, callback, immediatelyInvoked = true) => {
+  if (typeof times !== 'number') throw 'do-times expected the first argument [times] to be a number'
+  if (typeof callback !== 'function') throw 'do-times expected the second argument [callback] to be a function'
+  if (!immediatelyInvoked && typeof immediatelyInvoked !== 'boolean') throw 'do-times expected the third argument [immediatelyInvoked] to be a boolean'
 
-	if (typeof times !== 'number') {
-		throw 'do-times expected the first argument to be a number'
-	}
+  if (!immediatelyInvoked) return () => loop(times, callback)
 
-	if (typeof callback !== 'function') {
-		throw 'do-times expected the second argument to be a function'
-	}
-
-	if (!!constructor && typeof constructor !== 'boolean') {
-		throw 'do-times expected the third argument to be a boolean'
-	}
-
-	if (!constructor) {
-		loop(times, callback)
-	}
-
-	return (...args) => loop(times, callback, ...args)
+  loop(times, callback)
 }
 
 module.exports = doTimes

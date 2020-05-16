814600978255b460d5538ee25cf501c0a3181bf0
zentus <zentus@cimantra.com>
2020-05-16 02:56:41 +0200

    Remove internal currying

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const loop = (times, callback) => {
   }
 }
 
-const doTimes = (times, callback, immediatelyInvoked = true) => {
+const doTimes = (times, callback) => {
   if (typeof times !== 'number') throw 'do-times expected the first argument [times] to be a number'
   if (typeof callback !== 'function') throw 'do-times expected the second argument [callback] to be a function'
-  if (!immediatelyInvoked && typeof immediatelyInvoked !== 'boolean') throw 'do-times expected the third argument [immediatelyInvoked] to be a boolean'
-
-  if (!immediatelyInvoked) return () => loop(times, callback)
 
   loop(times, callback)
 }
